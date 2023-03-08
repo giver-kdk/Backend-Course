108486d819c0cc6e0acde35a75ff0358f7bba765
Revig <45360605+giver-kdk@users.noreply.github.com>
2023-03-08 18:12:11 +0545

    Prototypes in JS

---
+++ Codes/JS-Basics/Prototype.js
@@ -0,0 +1,16 @@
+/*
+	Prototype: 
+	It is about pre-concept of data manipulation features generally like:
+	methods and properties with array and objects
+	i.e; one property of array: length (ex: arr.length)
+	one method of array: push(ex: arr.push(args...))
+
+*/
+
+// To check built-in prototypes of array and object, we can do: 
+// See i nbrowser for detailed prototypes like: forEach, map, indexOf, ...etc
+console.log(Array());
+console.log(Object());
+
+// We can create our own prototype as well
+
